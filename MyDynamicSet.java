9bdd5562649636afba6041f321eff6928773b989
Roni Nahum <roninahum13@gmail.com>
2026-04-20 17:56:36 +0300

    Task 3

--- MyDynamicSet.java
+++ MyDynamicSet.java
@@ -2,14 +2,7 @@
  * @param <T> The type of the satellite data of the elements in the dynamic-set.
  */
 public class MyDynamicSet<T> {
-	/*
-     * You may add any fields that you wish to add.
-     * Remember that the use of built-in Java classes is not allowed,
-     * the only variables types you can use are: 
-     * 	-	the given classes in the assignment
-     * 	-	basic arrays
-     * 	-	primitive variables
-     */
+	private MySortedLinkedList set;
 
 
 	/**
@@ -17,34 +10,54 @@ public class MyDynamicSet<T> {
 	 * @param N The maximum number of elements in the dynamic set at each time.
 	 */
 	public MyDynamicSet(int N) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
-	}
+        this.set = new MySortedLinkedList();
+    }
 	
 	public Element<T> search(int k) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		if (set.head() == null) {
+            return null;
+        }
+        else {
+            ListLink currElement = set.head();
+            while (currElement != null){
+                if (currElement.key() == k){
+                    return (Element<T>) currElement;
+                }
+                else if (currElement.key() > k) {
+                    return null;
+                }
+                else {
+                    currElement = currElement.getNext();
+                }
+            }
+        }
+        return null;
 	}
 	
 	public void insert(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		ListLink newX = new ListLink(x.key(), x.satelliteData());
+        this.set.insert(newX);
 	}
 	
 	public void delete(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		this.set.delete((ListLink) x);
 	}
 	
 	public Element<T> minimum() {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		return set.head();
 	}
 	
 	public Element<T> maximum() {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		return set.tail();
 	}
 	
 	public Element<T> successor(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		ListLink newX = (ListLink) x;
+        return newX.getNext();
 	}
 	
 	public Element<T> predecessor(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+        ListLink newX = (ListLink) x;
+        return newX.getPrev();
 	}
 }
